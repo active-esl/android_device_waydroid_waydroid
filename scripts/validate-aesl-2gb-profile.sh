20d39e5a174a64225fb33089755d147e38fd2aba
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2026-09-07 14:04:10 +0100

    fix(aesl): include 2GB target in ARM64 setup

--- scripts/validate-aesl-2gb-profile.sh
+++ scripts/validate-aesl-2gb-profile.sh
@@ -6,6 +6,7 @@ products="${repo_root}/AndroidProducts.mk"
 product="${repo_root}/waydroid_aesl_2gb_arm64_only/lineage_waydroid_aesl_2gb_arm64_only.mk"
 properties="${repo_root}/configs/system-2gb.prop"
 board="${repo_root}/waydroid_aesl_2gb_arm64_only/BoardConfig.mk"
+android_mk="${repo_root}/Android.mk"
 
 require_text() {
     local file="$1" text="$2"
@@ -22,6 +23,7 @@ require_text "${product}" 'ANDROID_USE_WIDEVINE := false'
 require_text "${product}" 'ANDROID_USE_NDK_TRANSLATION := false'
 require_text "${product}" 'ro.config.low_ram=true'
 require_text "${board}" 'waydroid_arm64_only/BoardConfig.mk'
+require_text "${android_mk}" 'waydroid_aesl_2gb_arm64_only'
 require_text "${properties}" 'ro.lmk.use_psi=true'
 require_text "${properties}" 'ro.lmk.use_minfree_levels=false'
 require_text "${properties}" 'ro.surface_flinger.supports_background_blur=0'
